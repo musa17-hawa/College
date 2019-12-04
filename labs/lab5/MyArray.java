882789daccae462e96136b1e901ab31be0f11d96
Musa Abu El-Hawa <musahawa3@gmail.com>
2019-12-04 16:15:38 +0200

    lab5 added

---
+++ labs/lab5/MyArray.java
@@ -0,0 +1,65 @@
+public class MyArray{
+	private int[] numbers;
+
+	public MyArray(int n){
+		this.numbers = new int[n];
+		for(int i = 0; i < n; i++){
+			this.numbers[i] = (int)(Math.random()*100);
+		}
+	}
+	public MyArray(int[] other){
+		this.numbers = new int[other.length];
+		for(int i = 0; i < other.length; i++){
+			this.numbers[i] = other[i];
+		}
+	}
+
+	public String toString(){
+		String result = "{";
+		for(int i = 0; i < this.numbers.length; i++){
+			result +=this.numbers[i];
+			result += (i < this.numbers.length - 1) ? ", " : "";
+		}
+		result += "}";
+		return result;
+	}
+
+	public int findMaxNum(){
+		int max = 0;
+		for(int i = 0; i < this.numbers.length; i++){
+			max = (this.numbers[i] > max) ? this.numbers[i] : max;
+		}
+		return max;
+	}
+
+	public boolean hasSubArray(int[] other){
+	// 	for(int i = 0; i <=(this.numbers.length - other.length); i++){
+	// 		System.out.println(i);
+	// 		for(int k = 0; k < other.length && this.numbers[i + k] == other[k]; k++){
+	// 			System.out.println(k);
+	// 			if(k+1 >= other.length){
+	// 				return true;
+	// 			}
+	// 		}
+	// 	}
+	// return false;
+	// }
+
+		int i = 0;
+		int k = 0;
+		while(i < this.numbers.length && k < other.length){
+			if(this.numbers[i] == other[k]){
+				i++;
+				k++;
+				if(k == other.length){
+					return true;
+				}
+			}
+			else{
+				i++;
+				k = 0;
+			}
+		}
+		return false;
+	}
+}
---
+++ labs/lab5/Tester.java
@@ -0,0 +1,19 @@
+public class Tester{
+	public static void main(String[] args) {
+		MyArray array = new MyArray(5);
+
+		int[] a = {1,2,3,1,2,4};
+		int[] b = {1,2,4};
+
+		MyArray arraya = new MyArray(a);
+		MyArray arrayb = new MyArray(b);
+
+
+		System.out.println(array);
+		System.out.println("max is "+array.findMaxNum());
+		System.out.println(arraya);
+		System.out.println(arrayb);
+		System.out.println(arraya.hasSubArray(b));
+
+	}
+}
