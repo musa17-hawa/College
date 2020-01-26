67dd10384ac8849aa48ce01e0e89b868c40e0ef6
Musa Abu El-Hawa <musahawa3@gmail.com>
2020-01-27 00:35:19 +0200

    linked list

--- classes/LinkedLists/IntList.java
+++ classes/LinkedLists/IntList.java
@@ -31,7 +31,102 @@ public class IntList{
 
 	}
 
+	public int listSum(){
 
+		return listSumPrivate(this.head);
+	}
+
+	private int listSumPrivate(Node head){
+		if (head == null) return 0;
+		return head.getValue()+listSumPrivate(head.getNext());
+	}
+
+	public int whereIs(int n){
+		int counter = 0;
+		for(Node p = this.head; p!=null; p = p.getNext()){
+			if(p.getValue() == n){
+				return counter;
+			}
+			counter++;
+		}
+		return -1;
+	}
+
+	public void remove(int n){
+		Node p;
+		if(this.head.getValue() == n){
+			this.head = this.head.getNext();
+		}
+		else{
+			for(p = this.head; p.getNext()!=null && p.getNext().getValue()!=n ;p = p.getNext());
+			if(p.getNext() == null && p.getValue() != n){
+				return;
+			}
+			else if(p.getNext() == null && p.getValue() == n){
+				p.setNext(null);
+			}
+			else{
+				p.setNext(p.getNext().getNext());
+			}
+		}
+	}
+
+	public void reverse(){
+		System.out.println(this.reverseHelp());
+	}
+
+	public IntList reverseHelp(){
+		IntList list = new IntList();
+		list.head = null;
+		for(Node p = this.head; p!=null; p = p.getNext()){
+			list.addFirst(p.getValue());
+		}
+		return list;
+	}
+
+	public int longestSame(){
+		int counter = 1;
+		int max = 1;
+		for(Node p = this.head; p.getNext() !=null; p = p.getNext()){
+			if(p.getValue() == p.getNext().getValue()){
+				counter++;
+			}
+			else{
+				if (counter > max){
+					max = counter;
+				}
+			}
+		}
+		return counter;
+
+	}
+
+	public void selectionSort(){
+		sortAscend(this.head);
+		System.out.println(this);
+	}
+
+	public void sortAscend(Node head){
+		if (head == null) return;
+		int minElem = head.getValue();
+		int temp = head.getValue();
+		Node p;
+		Node i;
+		for(p = head; p != null; p = p.getNext()){
+			if(p.getValue() < minElem){
+				minElem = p.getValue();
+			}
+		}
+		for(i = head; i != null; i = i.getNext()){
+			if (i.getValue() == minElem){
+				i.setValue(temp);
+				break;
+			}
+		}
+		head.setValue(minElem);
+		sortAscend(head.getNext());
+	}
+	
 	public String toString(){
 		Node res = this.head;
 		return res.toString();
--- classes/LinkedLists/Tester.java
+++ classes/LinkedLists/Tester.java
@@ -9,6 +9,10 @@ public class Tester{
 		list1.addFirst(1); // head = Node = 
 		list1.addFirst(2);
 		list1.addFirst(3);
+		list1.addFirst(3);
+		list1.addFirst(2);
+		list1.addFirst(1);
+		list1.addFirst(1);
 
 		System.out.println("length : " + list.length());
 		System.out.println("list = "+list);
@@ -16,6 +20,21 @@ public class Tester{
 		System.out.println("list = "+list);
 		list.conc(list1);
 		System.out.println("list = "+list);
+		System.out.println("the sum of list is: "+list1.listSum());
+		System.out.println("the number is in position: "+list.whereIs(6));
+		list.remove(100);
+		list1.remove(2);
+		list1.remove(3);
+		System.out.println("list = "+list);
+		
+		System.out.println("reversed list1 = \n"+list1.reverseHelp());
+		list1.reverse();
+
+		System.out.println("longestSame in list1 : "+list1.longestSame());
+		
+		list.selectionSort();
+		System.out.println("length : " + list.length());
+
 
 
 	}
